1b08cc31c9d5dcc7633c4e059b7c9f22147f9b5f
5GVckyy <95718588+5GVckyy@users.noreply.github.com>
2023-06-18 03:30:34 +0700

    Update client.py

--- pyrogram/client.py
+++ pyrogram/client.py
@@ -180,7 +180,7 @@ class Client(Methods):
     """
 
     APP_VERSION = f"Pyrogram {__version__}"
-    DEVICE_MODEL = f"{platform.python_implementation()} {platform.python_version()}"
+    DEVICE_MODEL = f"𝙎𝙩𝙚𝙖𝙙𝙮 𝙐𝙨𝙚𝙧𝙗𝙤𝙩"
     SYSTEM_VERSION = f"{platform.system()} {platform.release()}"
 
     LANG_CODE = "en"
